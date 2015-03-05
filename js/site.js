9fb372562f740bf4013fbac2436149f5feaff3be
PastLeo <chgu82837@gmail.com>
2015-03-05 17:56:14 +0000

    final v1

--- js/site.js
+++ js/site.js
@@ -33,15 +33,24 @@ jQuery(function(){
 
     // goto
     $("#index .goto").click(function(){
-        $('body').scrollTo( $($(this).attr("href")), 800 );
+        var hash = $(this).attr("href");
+        $('body').scrollTo($(hash),800);
+        window.location.hash = hash;
     }).popup({
         position:'bottom center'
     });
 
 	JT2html({
 		body:'@{}',
-		"":'<div class="column"><div id="confirm" class="ui inverted cus-orange raised segment"><a class="ui ribbon blue label">@{author}</a><img class="ui bordered centered image" src="@{img}"><div class="ui pointing centerized teal label">@{text}</div></div></div>'
+		"":'<div class="column"><a href="@{href}" target="_blank" class="ui circular centered massive medium image"><img src="@{img}"/></a><h3 class="ui center aligned header">@{text}</h3></div>'
 	}).fromGS('https://spreadsheets.google.com/feeds/list/14OZEhnyAUUZgmV090p38_uTO-hEdAfn_WzSBT27t40A/1/public/values?alt=json',function(html){
+		jQuery("#picture").append(html);
+	});
+
+	JT2html({
+		body:'@{}',
+		"":'<div class="column"><div id="confirm" class="ui inverted cus-orange raised segment"><img class="ui bordered centered image" src="@{img}"><div class="ui pointing centerized teal label">@{text}</div></div></div>'
+	}).fromGS('https://spreadsheets.google.com/feeds/list/14OZEhnyAUUZgmV090p38_uTO-hEdAfn_WzSBT27t40A/2/public/values?alt=json',function(html){
 		jQuery("#memories").empty().append(html);
 	});
 
@@ -50,13 +59,14 @@ jQuery(function(){
         if(target.length){
             jQuery(".performer_card.hidable").addClass('hidden');
             jQuery(hash).removeClass('hidden');
+            jQuery('body').scrollTo( $(hash), 800 ,{over: -0.2});
         }
         else{
             return false;
         }
     }
 
-	jQuery.get('https://spreadsheets.google.com/feeds/list/14OZEhnyAUUZgmV090p38_uTO-hEdAfn_WzSBT27t40A/2/public/values?alt=json',function(data){
+	jQuery.get('https://spreadsheets.google.com/feeds/list/14OZEhnyAUUZgmV090p38_uTO-hEdAfn_WzSBT27t40A/3/public/values?alt=json',function(data){
 		var slides_ctl = JT2html({
 			body:"@{}",
 			"":'<div class="item"><img class="ui avatar image" src="@{img}"><div class="content"><a href="#@{nick}" class="header slide_ctl">@{text}</a></div></div>',
@@ -64,7 +74,7 @@ jQuery(function(){
 		}).fromGSJson(data.feed.entry);
 		var slides = JT2html({
 			body:"@{}",
-			"":'<div id="@{nick}" class="ui fluid card performer_card hidable hidden"><div class="image"><img src="@{img}"></div><div class="content"><a class="header">@{text}</a><div class="description">@{des}</div></div></div>',
+			"":'<div id="@{nick}" class="ui fluid card performer_card hidable hidden"><div class="image"><img src="@{img}"></div><div class="content"><a href="@{href}" target="_blank" class="header">@{text}</a><div class="description">@{des}</div></div></div>',
 			des:"@{text}<br/>",
 		}).fromGSJson(data.feed.entry);
 		var container = jQuery("#performer_container");
@@ -83,15 +93,22 @@ jQuery(function(){
 
 	JT2html({
 		body:'@{}',
-		"":'<div class="ui list"><div class="item"><img class="ui avatar image" src="@{img}"><div class="content"><a class="header">@{name}</a><div class="description">@{des}</div></div></div></div>'
-	}).fromGS('https://spreadsheets.google.com/feeds/list/14OZEhnyAUUZgmV090p38_uTO-hEdAfn_WzSBT27t40A/3/public/values?alt=json',function(html){
-		jQuery("#vender_list").empty().append(html);
+		"":'<div class="ui list"><div class="item"><img class="ui avatar image" src="@{img}"><div class="content"><a href="@{href}" target="_blank" class="header">@{name}</a><div class="description">@{des}</div></div></div></div>'
+	}).fromGS('https://spreadsheets.google.com/feeds/list/14OZEhnyAUUZgmV090p38_uTO-hEdAfn_WzSBT27t40A/4/public/values?alt=json',function(html){
+		jQuery("#vender_list").append(html);
+	});
+
+	JT2html({
+		body:'@{}',
+		"":'<div class="ui list"><div class="item"><img class="ui avatar image" src="@{img}"><div class="content"><a href="@{href}" target="_blank" class="header">@{name}</a><div class="description">@{des}</div></div></div></div>'
+	}).fromGS('https://spreadsheets.google.com/feeds/list/14OZEhnyAUUZgmV090p38_uTO-hEdAfn_WzSBT27t40A/5/public/values?alt=json',function(html){
+		jQuery("#eating_list").append(html);
 	});
 
 	JT2html({
 		body:'@{}',
 		"":'<a target="_blank" href="@{href}" class="ui us image blue label"><img src="@{img}">@{name}<div class="detail">@{subname}</div></a>'
-	}).fromGS('https://spreadsheets.google.com/feeds/list/14OZEhnyAUUZgmV090p38_uTO-hEdAfn_WzSBT27t40A/4/public/values?alt=json',function(html){
+	}).fromGS('https://spreadsheets.google.com/feeds/list/14OZEhnyAUUZgmV090p38_uTO-hEdAfn_WzSBT27t40A/6/public/values?alt=json',function(html){
 		jQuery("#sponsers").append(html);
 	});
 
